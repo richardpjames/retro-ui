3d66515ccd13b34b17835ffa1d84c558397a1118
Richard James <richard@richardpjames.com>
2020-07-02 21:28:04 +0100

    Allow creation of boards from templates

--- src/components/BoardList/BoardListDetails.js
+++ src/components/BoardList/BoardListDetails.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 const BoardListDetails = (props) => {
 
@@ -7,17 +8,34 @@ const BoardListDetails = (props) => {
   }
 
   return (
-    <div className="card my-3 p-2 shadow-sm">
-      <div className="card-body">
-        <div className="row">
-          <div className="col-8">
-            <h5>{props.board.name}</h5>
-            <p>{props.board.description}</p>
+    <div>
+      <div className="modal fade" id={`modal-${props.board.boardId}`} tabIndex="-1" role="dialog">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Delete {props.board.name}?</h5>
+            </div>
+            <div className="modal-body">
+              <p>Are you sure you want to delete this board? There is no way to undo this later.</p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+              <button type="button" className="btn btn-danger" data-dismiss="modal" onClick={handleDelete}>Delete</button>
+            </div>
           </div>
-          <div className="col-4 align-middle text-right">
-            <button className="btn btn-primary align-middle">View</button>
-            <button className="btn btn-danger" onClick={handleDelete}>Delete</button>
-          </div>
+        </div>
+      </div>
+
+      <div className="card my-3 p-2 shadow-sm">
+        <div className="card-body">
+
+          <h5>{props.board.name}</h5>
+          <p className="small">Created {moment(props.board.created).format('DD/MM/YYYY HH:mm')}</p>
+          <p>{props.board.description}</p>
+
+          <button className="btn btn-primary align-middle">View</button>
+          <button className="btn btn-danger ml-3" data-toggle="modal" data-target={`#modal-${props.board.boardId}`}>Delete</button>
+
         </div>
       </div>
     </div>
--- src/components/BoardList/Boards.js
+++ src/components/BoardList/Boards.js
@@ -69,7 +69,7 @@ const Boards = () => {
         axios.delete(`${process.env.REACT_APP_API_URL}/api/boards/${boardId}`, config)
           // Overwrite the boards object with the results
           .then((response) => {
-            let updatedBoards = boards.filter(( board ) => {
+            let updatedBoards = boards.filter((board) => {
               return board.boardId !== boardId;
             });
             updateBoards(updatedBoards);
@@ -85,10 +85,21 @@ const Boards = () => {
     <h1>Your Boards</h1>
     <p>View an existing board, or create a new one. Your most recent boards are shown
       at the top of the page.</p>
-    <h3>Create New</h3>
-    <NewBoard addBoard={addBoard} />
-    <h3>Existing Boards</h3>
-    <BoardList boards={boards} removeBoard={removeBoard}/>
+    <div className="row">
+      <div className="col-lg-6">
+        <h3>Create New</h3>
+        <p>Creating a new board is as simple as picking a name, giving a description and then
+        choosing one of the starter templates to get going. Once the board is created you can
+        add/change the columns and descriptions.</p>
+        <NewBoard addBoard={addBoard}/>
+      </div>
+      <div className="col-lg-6">
+        <h3>Existing Boards</h3>
+        <p>These are the boards from your previous retrospectives, which are kept here until you
+        choose to delete them.</p>
+        <BoardList boards={boards} removeBoard={removeBoard}/>
+      </div>
+    </div>
   </div>);
 }
 
