3e84445167d704f0412c472edd90825be2448c41
Richard James <richard@Richards-iMac.local>
2020-08-19 17:42:04 +0100

    Expand footer

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -2,15 +2,48 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer = (props) => {
+  const scrollTop = (event) => {
+    if (event.target.getAttribute('href')) {
+      window.scroll(0, 0);
+    }
+  };
+
   return (
-    <footer className="footer">
-      <div className="content has-text-centered">
+    <footer className="footer" onClick={scrollTop}>
+      <div className="content has-text-left">
         <p>
           <strong>RetroSpectacle.io</strong>
         </p>
-        <p>
-          <Link to="/privacy">Privacy Policy</Link>
-        </p>
+        <div className="columns">
+          <div className="column is-one-fifth">
+            <ul>
+              <li>
+                <Link to="/blog/posts/getting-started-with-retrospectacle">
+                  Getting Started
+                </Link>
+              </li>
+              <li>
+                <Link to="/privacy">Privacy Policy</Link>
+              </li>
+              <li>
+                <a href="mailto:support@retrospectacle.io">Contact Support</a>
+              </li>
+            </ul>
+          </div>
+          <div className="column is-one-fifth">
+            <ul>
+              <li>
+                <a
+                  href="https://twitter.com/spectacle_retro"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Twitter
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </footer>
   );
